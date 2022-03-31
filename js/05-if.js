1f7b88784f51d7d23b3d41a43f16eef73cbddd05
Dalius Ram <ramonas.dalius@gmail.com>
2022-03-31 11:44:12 +0300

    functions

---
+++ js/05-if.js
@@ -0,0 +1,21 @@
+/*
+IF
+
+Sablonai:
+if () {}
+if () {} else {}
+if () {} else if {} else {}
+
+Palyginimo operatoriai:
+- visi >, <, <=, >=, ==, !=, grieztos lygybes ===, gieztos nelygybes !==
+- naudotini >, <, <=, >=, grieztos lygybes ===, gieztos nelygybes !==
+- NEnaudotini !=, ==
+*/
+const a = 7;
+const b = 9;
+
+if (a > b) {
+    console.log("A yra daugiau uz B");
+} else {
+    console.log("B daugiau uz A");
+}
---
+++ js/06-function.js
@@ -0,0 +1,58 @@
+// visi mokiniai turi tik 4 pazymius ir reikia suskaiciuoti vidurki
+const petras = [10, 10, 8, 2];
+const maryte = [5, 9, 7, 6];
+const jonas = [7, 7, 8, 2];
+const ona = [1, 5, 8, 10];
+
+
+function vidurkis(pazymiuSarasas) {
+    let pazymiuSuma = 0;
+    pazymiuSuma += pazymiuSarasas[0];
+    pazymiuSuma += pazymiuSarasas[1];
+    pazymiuSuma += pazymiuSarasas[2];
+    pazymiuSuma += pazymiuSarasas[3];
+    return pazymiuSuma / pazymiuSarasas.length; // returnas funkcijoje gali buti tik 1. funkcija visada turi tureti return!!!
+}
+
+const petroVidurkis = vidurkis(petras);
+console.log('Petras: ', petroVidurkis);
+
+const marytesVidurkis = vidurkis(maryte);
+console.log('Maryte: ', marytesVidurkis);
+
+const jonoVidurkis = vidurkis(jonas);
+console.log('Jonas: ', jonoVidurkis);
+
+const onosVidurkis = vidurkis(ona);
+console.log('Ona: ', onosVidurkis);
+
+
+/*
+Funkcija gauna varda ir turi grazinti sakini:
+"Vardo pirma raide yra [V] ir vardas sudarytas is [5] raidziu."
+*/
+
+function vardoAnalize(name) {
+
+    return `Vardo "${name}" pirma raide yra ${name[0]} ir vardas sudarytas is ${name.length} raidziu.`;
+}
+
+const marytesVardas = vardoAnalize('Maryte');
+console.log(marytesVardas);
+const onosVardas = vardoAnalize('Ona');
+console.log(onosVardas);
+
+// Funkcija gauna du skaicius ir turi grazinti lygti su sprendimu pvz:
+// 2 + 2 = 4
+
+function suma(a, b) {
+    return `${a} + ${b} = ${Math.round((a + b) *100)/ 100}`;
+}
+const s1 = suma(2, 3);
+console.log(s1);
+
+const s2 = suma(0.1, 0.2);
+console.log(s2);
+
+const s3 = suma(-88, -11);
+console.log(s3);
