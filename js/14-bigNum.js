d52f40bb691c5f29ff175bd7b5a04820dfdbd444
Dalius Ram <ramonas.dalius@gmail.com>
2022-04-05 12:26:39 +0300

    BiggestNum finished with all tests

--- js/14-bigNum.js
+++ js/14-bigNum.js
@@ -1,27 +1,44 @@
 function didziausiasSkaiciusSarase(list) {
+    // VALIDATION
     if (typeof list !== 'object') {
         return 'ERROR: netinkamas tipas';
     }
     if (list.length === 0) {
-        return 'ERROR: Sarasas negali buti tuscias';
+        return 'ERROR: sarasas negali buti tuscias'
     }
-    let biggestNumber = list[0];
+
+    // login
+    let biggestNumber = -Infinity;
 
     for (let i = 0; i < list.length; i++) {
         const number = list[i];
+        // Inner validation
+        if (typeof number !== 'number' || !isFinite(number)) {
+            continue; // permeta for`a i sekancia ciklo iteracija
+        }
+        //  inner logic
         if (number > biggestNumber) {
             biggestNumber = number;
         }
+        
+    // result validation
     }
-
+    if (biggestNumber === -Infinity) {
+        return 'Error: sarase nera nei vieno normalaus skaiciaus'
+    }
+    // result return
     return biggestNumber;
-}
+};
 console.log( didziausiasSkaiciusSarase('pomidoras'), '-->', 'pateikta netinkama tipo reiksme');
 console.log( didziausiasSkaiciusSarase([]),'-->', 'Sarasas negali buti tuscias');
-
 console.log( didziausiasSkaiciusSarase([5]), '-->', 5);
 console.log( didziausiasSkaiciusSarase([1, 2, 3]), '-->', 3);
 console.log( didziausiasSkaiciusSarase([1, 3, 2]), '-->', 3);
 console.log( didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]), '-->', 75);
 console.log( didziausiasSkaiciusSarase([69, 69, 69, 69, 66]), '-->', 69);
 console.log( didziausiasSkaiciusSarase([ -1, -2, -3, -4, -5, -6, -7, -8]), '-->', -1);
+console.log( didziausiasSkaiciusSarase(['x', 1, 2, 3]), '-->', 3);
+
+console.log( didziausiasSkaiciusSarase(['x']), '-->', 'ERROR');
+console.log( didziausiasSkaiciusSarase(['x', true, [], -Infinity, Infinity]), '-->', 'ERROR');
+console.log( didziausiasSkaiciusSarase([true, -1]), '-->', -1);
